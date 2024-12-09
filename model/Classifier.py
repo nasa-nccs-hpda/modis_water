b0e62e5db1e56c6f887d774c86892d578f5c54ae
R <R>
2024-12-09 10:04:33 -0500

    Changed data type

--- model/Classifier.py
+++ model/Classifier.py
@@ -127,13 +127,7 @@ class Classifier(object):
     # -------------------------------------------------------------------------
     def _createOutputImage(self, name, predictions):
 
-        # ---
-        # Please allow this exception from PEP rules.  Splitting this into
-        # 79-character lines and using line continuations will make this mess
-        # of quotes even trickier.
-        # ---
-        MODIS_SINUSOIDAL_6842 = SpatialReference(
-            'PROJCS["Sinusoidal",GEOGCS["GCS_Undefined",DATUM["Undefined",SPHEROID["User_Defined_Spheroid",6371007.181,0.0]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Sinusoidal"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],UNIT["Meter",1.0]]')
+        MODIS_SINUSOIDAL_6842 = SpatialReference(            'PROJCS["Sinusoidal",GEOGCS["GCS_Undefined",DATUM["Undefined",SPHEROID["User_Defined_Spheroid",6371007.181,0.0]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Sinusoidal"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],UNIT["Meter",1.0]]')  # noqa: E501
 
         driver = gdal.GetDriverByName('GTiff')
 
